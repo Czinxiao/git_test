3a5f3fe6100767bd68b0f02d6a4bbea179a96e88
xiao <xiao1111.com>
2023-03-13 15:14:09 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("under first revision");
         System.out.println("hot-fix  Hello Git!");
-
+        System.out.println("master test");
     }
 }
 
