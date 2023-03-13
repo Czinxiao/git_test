aebfea1d28e279beec2d517c0e7ea930915a4c37
xiao <xiao1111.com>
2023-03-13 15:07:19 +0800

    hotFix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("under first revision");
+        System.out.println("hot-fix  Hello Git!");
 
     }
 }
+
